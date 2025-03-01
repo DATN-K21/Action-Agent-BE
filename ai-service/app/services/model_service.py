fb52201b1a0fa9ffb56812a5478ec64e712d5534
Nguyen Tuan Dat <nguyentuandat2k3cmg@gmail.com>
2025-03-01 21:23:39 +0700

    update model

--- ai-service/app/services/model_service.py
+++ ai-service/app/services/model_service.py
@@ -3,10 +3,10 @@ from pydantic import SecretStr
 
 from app.core.settings import env_settings
 
-MAX_TOKENS = 20000
+MAX_TOKENS = 10000
 
 
-def get_openai_model(model: str = "gpt-4o", temperature=0, streaming=True):
+def get_openai_model(model: str = "gpt-3.5-turbo", temperature=0, streaming=True):
     """Get an OpenAI language model."""
     api_key = SecretStr(env_settings.OPENAI_API_KEY)
     return ChatOpenAI(temperature=temperature, streaming=streaming, model=model, api_key=api_key)
