ab2c3acf328a9619890f4c32a6a00601999c46dd
Nguyễn Đình Ánh <94167719+git03-Nguyen@users.noreply.github.com>
2025-03-09 19:01:41 +0700

    Change response format of get-history (#19)

--- ai-service/app/schemas/history.py
+++ ai-service/app/schemas/history.py
@@ -9,4 +9,8 @@ from app.schemas.base import BaseResponse
 ########### RESPONSE SCHEMAS #####################
 ##################################################
 class GetHistoryResponse(BaseResponse):
-    messages: list[dict[str, Any]] = Field(..., title="List of messages", examples=[{"human": "Hello", "ai": "Hi"}])
+    messages: list[dict[str, Any]] = Field(
+        ...,
+        title="List of messages",
+        examples=[{"role": "human", "content": "Hello"}, {"role": "ai", "content": "Hi"}],
+    )
