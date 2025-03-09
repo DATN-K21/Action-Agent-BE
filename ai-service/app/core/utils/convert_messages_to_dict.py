d0253fc911ba783c8c3f4532b90bf5eae2e72813
git03-Nguyen <nguyendinhanhvlqt@gmail.com>
2025-03-09 18:59:46 +0700

    Change get-history response format

--- ai-service/app/core/utils/convert_messages_to_dict.py
+++ ai-service/app/core/utils/convert_messages_to_dict.py
@@ -8,9 +8,9 @@ from app.core.enums import MessageName
 def convert_messages_to_dicts(messages: Sequence[BaseMessage]) -> Sequence[Dict[str, Any]]:  # type: ignore
     for message in messages:
         if isinstance(message, HumanMessage):
-            yield {"human": message.content}  # type: ignore
+            yield {"role": "human", "content": message.content}  # type: ignore
         if isinstance(message, AIMessage):
             if message.name == MessageName.AI:
-                yield {"ai": message.content}  # type: ignore
+                yield {"role": "ai", "content": message.content}  # type: ignore
             if message.name == MessageName.TOOL:
-                yield {"tool": message.content}  # type: ignore
+                yield {"role": "tool", "content": message.content}  # type: ignore
