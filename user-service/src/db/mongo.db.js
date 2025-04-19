ac33c87a3b1bf6ff5caf12458685b21899013245
Nguyễn Đình Ánh <94167719+git03-Nguyen@users.noreply.github.com>
2025-04-19 11:33:40 +0000

    Add seeds for roles in user-service

--- user-service/src/db/mongo.db.js
+++ user-service/src/db/mongo.db.js
@@ -2,13 +2,15 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 const { mongoDB: mongoConfig } = require("../configs/db.config");
+const seedRoles = require("../modules/role/role.seed");
 
 const { maxPoolSize: MAX_POOL_SIZE } = mongoConfig;
 const connectString = process.env.MONGODB_CONNECTION_STRING;
 
 const setupMongoDB = async () => {
     mongoose.connect(connectString, { maxPoolSize: MAX_POOL_SIZE })
-        .then(_ => {
+        .then(async () => {
+            await seedRoles(mongoose);
             console.log("Connect to MongoDB successfully");
         }).catch(err => console.log("Error connecting to MongoDB: " + err))
 }
