c96b85452802a1a9c05c3b31625d65b6e5329214
Triệu Hoàng Thiên Ân <thienan2003bt@gmail.com>
2025-05-27 23:48:49 +0700

    Basic setup for building Docker for extension service

--- api-gateway/src/configs/endpoint.config.js
+++ api-gateway/src/configs/endpoint.config.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const ENDPOINT_CONFIGS = {
     USER_SERVICE_URL: process.env.USER_SERVICE_URL,
     AI_SERVICE_URL: process.env.AI_SERVICE_URL,
+    EXTENSION_SERVICE_URL: process.env.EXTENSION_SERVICE_URL,
     RABBITMQ_URL: process.env.RABBITMQ_URL,
     ELASTICSEARCH_URL: process.env.ELASTICSEARCH_URL,
     REDIS_URL: process.env.REDIS_URL,
--- api-gateway/src/routes/api.route.js
+++ api-gateway/src/routes/api.route.js
@@ -8,6 +8,7 @@ const privateEndpointMiddleware = require('../middlewares/privateEndpoint.middle
 const serviceRegistry = {
     'user': ENDPOINT_CONFIGS.USER_SERVICE_URL,
     'ai': ENDPOINT_CONFIGS.AI_SERVICE_URL,
+    'extension:': ENDPOINT_CONFIGS.EXTENSION_SERVICE_URL,
 };
 
 Object.entries(serviceRegistry).forEach(([serviceName, target]) => {
