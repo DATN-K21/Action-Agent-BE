a25c05e105f8aef4cc00965c750e5fcb7d1c8d21
Nguyen Tuan Dat <nguyentuandat2k3cmg@gmail.com>
2025-05-29 15:26:25 +0700

    Fix db, now can run project

--- ai-service/app/db_models/member.py
+++ ai-service/app/db_models/member.py
@@ -1,18 +1,29 @@
-from sqlalchemy import Column, ForeignKey, String, UniqueConstraint
+from sqlalchemy import Column, ForeignKey, String, UniqueConstraint, Numeric
 from sqlalchemy.orm import relationship
 
-from app.core.models import MemberBase
 from app.db_models.base_entity import BaseEntity
 
 
-class Member(MemberBase, BaseEntity):
+class Member(BaseEntity):
     __tablename__ = "members"
     __table_args__ = (
         UniqueConstraint("name", "belongs_to", name="unique_team_and_name"),
     )
 
-    name = Column(String, nullable=False)
+    name = Column(String(64), unique=True, nullable=False)
     belongs_to = Column(String, ForeignKey("teams.id"), nullable=False)
+
+    backstory = Column(String, nullable=True)
+    role = Column(String, nullable=True)
+    type = Column(String, nullable=True)  # one of: leader, worker, freelancer
+    owner_of = Column(String, nullable=True)
+    position_x = Column(Numeric, nullable=True)
+    position_y = Column(Numeric, nullable=True)
+    source = Column(String, nullable=True)
+    provide = Column(String, nullable=True)
+    model = Column(String, nullable=True)
+    temperature = Column(Numeric, nullable=True)
+    interrupt = Column(Numeric, nullable=True, default=False)
 
     belongs = relationship("Team", back_populates="members")
 
--- ai-service/app/db_models/team.py
+++ ai-service/app/db_models/team.py
@@ -1,15 +1,17 @@
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
-from app.core.models import TeamBase
 from app.db_models.base_entity import BaseEntity
 
 
-class Team(TeamBase, BaseEntity):
+class Team(BaseEntity):
     __tablename__ = "teams"
 
     name = Column(String(64), unique=True, nullable=False)
     owner_id = Column(String, ForeignKey("users.id"), nullable=False)
+
+    description = Column(String, nullable=True)
+    icon = Column(String, nullable=True)  # Add an icon field for the team
 
     # Relationships
     owner = relationship("User", back_populates="teams")
