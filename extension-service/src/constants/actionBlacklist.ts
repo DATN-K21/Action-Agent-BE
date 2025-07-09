35a280f4066525a9a360ca4b808613a1e6907ab1
Triệu Hoàng Thiên Ân <thienan2003bt@gmail.com>
2025-07-09 18:24:13 +0700

    Fix extension service apps API (#80)

---
+++ extension-service/src/constants/actionBlacklist.ts
@@ -0,0 +1,42 @@
+export const ACTION_BLACKLIST: string[] = [
+  "False",
+  "None",
+  "True",
+  "and",
+  "as",
+  "assert",
+  "async",
+  "await",
+  "break",
+  "class",
+  "continue",
+  "def",
+  "del",
+  "elif",
+  "else",
+  "except",
+  "finally",
+  "for",
+  "from",
+  "global",
+  "if",
+  "import",
+  "in",
+  "is",
+  "lambda",
+  "nonlocal",
+  "not",
+  "or",
+  "pass",
+  "raise",
+  "return",
+  "try",
+  "while",
+  "with",
+  "yield",
+];
+
+export const ACTION_BLACKLIST_QUERY: Object = {};
+ACTION_BLACKLIST.forEach(blacklistedName => {
+  ACTION_BLACKLIST_QUERY[`parameters.properties.${blacklistedName}`] = { $exists: true };
+});
