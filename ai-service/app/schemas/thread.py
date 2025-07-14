5e4347cadec10f2e8da88ce39b17a612764e8854
Nguyễn Đình Ánh <94167719+git03-Nguyen@users.noreply.github.com>
2025-07-14 08:41:23 +0000

    Remove validation in user

--- ai-service/app/schemas/thread.py
+++ ai-service/app/schemas/thread.py
@@ -11,12 +11,12 @@ from app.schemas.base import BaseRequest, BaseResponse, CursorPagingResponse
 ########### REQUEST SCHEMAS ######################
 ##################################################
 class CreateThreadRequest(BaseRequest):
-    title: str = Field(..., min_length=3, max_length=50)
+    title: str = Field(..., min_length=1, max_length=50)
     assistant_id: str = Field(..., min_length=3, max_length=50)
 
 
 class UpdateThreadRequest(BaseRequest):
-    title: Optional[str] = Field(None, min_length=3, max_length=50)
+    title: Optional[str] = Field(None, min_length=1, max_length=50)
     assistant_id: Optional[str] = Field(None, min_length=3, max_length=50)
 
 
